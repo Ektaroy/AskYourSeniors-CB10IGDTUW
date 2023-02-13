54db8041c2e6af8dc40f7095794bf304ff34b4ba
Ektaroy <97738792+Ektaroy@users.noreply.github.com>
2023-02-13 19:37:30 +0530

    Revert "Revert "home page updated""
    This reverts commit bc944e16865f1b30a8e62af45f458e7bba5c0286.

---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Homemain from "../components/Homemain";
+
+const home = () => {
+  return (
+    <>
+      <Navbar />
+      <Homemain />
+    </>
+  );
+};
+
+export default home;
